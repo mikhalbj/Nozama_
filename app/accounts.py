5e02c3075aafc098f1f8080d00ac75894e83df4f
Jack Proudfoot <jsp50@duke.edu>
2021-10-14 23:34:20 -0400

    Updated account page layout and changed id to uuid

---
+++ app/accounts.py
@@ -0,0 +1,21 @@
+from flask import render_template, redirect, url_for, flash, request
+from werkzeug.urls import url_parse
+from flask_login import login_user, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from flask_babel import _, lazy_gettext as _l
+
+from .models.user import User
+
+
+from flask import Blueprint
+bp = Blueprint('accounts', __name__)
+
+
+@bp.route('/account', methods=['GET'])
+def account():
+    if not current_user.is_authenticated:
+        return redirect(url_for('index.index'))
+    
+    return render_template('account.html', title='Account', purchase_history=[], balance=100.09)
