e8d9121624dea68799492d95dea6ed7d5b246d9d
Luke Evans <lae32@duke.edu>
2021-11-04 21:02:35 -0400

    add more robust search

--- app/models/product.py
+++ app/models/product.py
@@ -28,7 +28,15 @@ WHERE available = :available
                               available=available)
         return [Product(*row) for row in rows]
     
-    #ONLY HANDLES EXACT MATCHES
+    @staticmethod
+    def get_categories():
+        rows = app.db.execute('''
+        SELECT name
+        FROM Tag
+        ''')
+        print(rows)
+        return [row[0] for row in rows]
+    
     @staticmethod
     def search(strng):
         rows = app.db.execute('''
@@ -36,6 +44,35 @@ SELECT id, name, price, available
 FROM Product
 WHERE name LIKE :strng
 ''',
-                              strng=strng)
+                              strng="%"+strng+"%")
         return [Product(*row) for row in rows]
+
+    # THIS RETURNS NOTHING IF A PRODUCT HAS NO REVIEWS!!!
+    @staticmethod
+    def advanced_search(strng="", searchName=True, searchDesc=False, sortbyPrice=False, availOnly=False, priceMax=False, tag=False):
+
+    
+        sel = "SELECT Product.id, Product.name, Product.price, Product.available, COALESCE(AVG(Review.rating), NULL) AS rate"
+        gby = "GROUP BY Product.id"
+        where = "WHERE Review.id = ProductReview.review AND ProductReview.product = Product.id"
+        frm = "FROM Product, Review, ProductReview"
+        
+        if searchName:
+            where += " AND Product.name LIKE :strng"
+        if searchDesc:
+            where += " AND Product.description LIKE :strng"
+        if availOnly:
+            where += " AND Product.available = True"
+        if priceMax:
+            where += " AND Product.price < :pricemax"
+        if tag:
+            where += " AND ProductTag.tag = Tag.id AND Tag.name = :tag"
+            frm += ", Tag, ProductTag"
+        
+        qry = sel + "\n" + frm + "\n" + where + "\n" + gby
+        print(qry)
+        rows = app.db.execute(qry, strng="%"+strng+"%", tag=tag, pricemax=priceMax)
+        print(rows)
+        #return rows
+        return rows
 
--- app/product.py
+++ app/product.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, url_for, flash, request
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, RadioField, DecimalField, SelectMultipleField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from flask_babel import _, lazy_gettext as _l
 
@@ -14,25 +14,41 @@ bp = Blueprint('product', __name__)
 
 class SearchForm(FlaskForm):
     searchterm = StringField('Search here')
+    tag = RadioField('Filter by category')
+    avail = BooleanField('Only find available items')
+    maxprice = DecimalField('Only find items cheaper than:')
+    searchin = SelectMultipleField('Match ketwords in:', choices=[("description", "description"), ("description", "description")])
     submit = SubmitField()
 
 @bp.route('/product_details/<uuid:id>', methods=['GET'])
 def product(id):
-    
     product = Product.get(id)
     return render_template('product_details.html', title='See Product', product=product)
 
 @bp.route('/search/<argterm>', methods=['GET', 'POST'])
 def search(argterm):
-    
-    # SEARCH TERM CANNOT INCLUDE A SLASH
-    products = Product.search(argterm)
+    print(request.form)
+    products = Product.search(strng=argterm)
     return render_template('search.html', title='Search for Products', products=products, term=argterm)
+
+
+@bp.route('/advanced_search/', methods=['GET', 'POST'])
+def advanced_search():
+    s = request.args.get("argterm")
+    a = request.args.get("avail")
+    t = request.args.get("t")
+    p = request.args.get("maxprice")
+    d = request.args.get("searchdesc")
+    products = Product.advanced_search(strng=s, tag=t, priceMax=p, availOnly=a, searchDesc=d)
+    return render_template('search.html', title='Search for Products', products=products, term=s)
+
 
 @bp.route('/search/', methods=['GET', 'POST'])
 def presearch():
     form = SearchForm()
+    form.tag.choices = Product.get_categories()
     if form.is_submitted():
-        return redirect(url_for('product.search', argterm=form.searchterm.data))
+        print(request.form)
+        return redirect(url_for('product.advanced_search', argterm=form.searchterm.data, tag=form.tag.data, maxprice=form.maxprice.data, avail=form.avail.data))
     return render_template('search.html', title='Search for Products', presearch=True, form=form)
 
